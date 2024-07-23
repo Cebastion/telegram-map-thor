6186312a42523d682738bee8a1e7503dc475e290
Cebastion <kdimon2006@gmail.com>
2024-07-23 08:16:48 +0300

    update

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,12 @@
+import { YMaps } from '@pbe/react-yandex-maps'
+import { FC } from 'react'
+
+const Map: FC = () => {
+  return (
+    <YMaps>
+      <Map/>
+    </YMaps>
+  )
+}
+
+export default Map
